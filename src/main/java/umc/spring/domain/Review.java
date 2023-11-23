b2162db2ff6022785062e03217e998e5f866c9f1
hyeon <ryuoo0123@gmail.com>
2023-11-23 19:21:36 +0900

    Initial commit

---
+++ src/main/java/umc/spring/domain/Review.java
@@ -0,0 +1,26 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private Float score;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
---
+++ src/main/java/umc/spring/domain/enums/SocialType.java
@@ -0,0 +1,5 @@
+package umc.spring.domain.enums;
+
+public enum SocialType {
+    KAKO, GOOGLE, NAVER, APPLE
+}
